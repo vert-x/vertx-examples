25a20d370eb6688f8cd7fd0a5dabc73a9ba3c094
purplefox <timvolpe@gmail.com>
2013-10-01 07:31:50 +0100

    mem leak test for event bus bridge

---
+++ src/raw/java/ebb_memleak_test/BridgeServer.java
@@ -0,0 +1,42 @@
+package ebb_memleak_test;
+
+import org.vertx.java.core.Handler;
+import org.vertx.java.core.http.HttpServer;
+import org.vertx.java.core.http.HttpServerRequest;
+import org.vertx.java.core.json.JsonArray;
+import org.vertx.java.core.json.JsonObject;
+import org.vertx.java.core.logging.Logger;
+import org.vertx.java.core.sockjs.SockJSServer;
+import org.vertx.java.platform.Verticle;
+
+/**
+ * @author <a href="http://tfox.org">Tim Fox</a>
+ */
+public class BridgeServer extends Verticle {
+  Logger logger;
+
+  public void start() {
+    logger = container.logger();
+    
+    HttpServer server = vertx.createHttpServer();
+
+    // Also serve the static resources. In real life this would probably be done by a CDN
+    server.requestHandler(new Handler<HttpServerRequest>() {
+      public void handle(HttpServerRequest req) {
+        if (req.path().equals("/")) req.response().sendFile("ebb_memleak_test/index.html"); // Serve the index.html
+        if (req.path().endsWith("vertxbus.js")) req.response().sendFile("ebb_memleak_test/vertxbus.js"); // Serve the js
+      }
+    });
+
+    JsonArray permitted = new JsonArray();
+    permitted.add(new JsonObject()); // Let everything through
+
+    ServerHook hook = new ServerHook(logger);
+
+    SockJSServer sockJSServer = vertx.createSockJSServer(server);
+    sockJSServer.setHook(hook);
+    sockJSServer.bridge(new JsonObject().putString("prefix", "/eventbus"), permitted, permitted);
+
+    server.listen(8080);
+  }
+}
